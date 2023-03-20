4e5b1b1b9ecbbb528a1d69b0f889fa1c87a7dee0
kelsie kidd <kelsiekidd93@gmail.com>
2023-03-20 13:42:42 -0500

    retrieved data for occupation and states

---
+++ src/routes/SignUp.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from "svelte";
+    import { occData, occArrays, stateData, stateArrays } from "../store";
+
+    onMount(async () => {
+        fetch("https://frontend-take-home.fetchrewards.com/form")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                occData.set(data);
+                stateData.set(data);
+            })
+            .catch((error) => {
+                console.log(error);
+                return [];
+            });
+    });
+
+
+</script>
