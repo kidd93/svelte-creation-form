2d30006b133d3350b9632fa3d953a340e43f3667
kelsie kidd <kelsiekidd93@gmail.com>
2023-03-20 12:30:47 -0500

    Added userbase and appId

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Tailwindcss from "./Tailwindcss.svelte";
+    import { storeBase, storeUser, promise } from "./store";
+	
+	const userbase = window.userbase;
+	window.userbase = null;
+	
+	$storeBase = userbase;
+	$storeUser = null;
+	
+	$promise = userbase
+	.init({ appId: "2fdb8270-a267-44ff-b305-5d01ffc90505" })
+	.then((session) => ($storeUser = session.user));
+	
+	
+</script>
+<Tailwindcss />
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { writable} from 'svelte/store';
+
+export const storeBase = writable(null);
+export const promise = writable(null);
+export const storeUser = writable(null);
